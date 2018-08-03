170869208114413c215a455b3e54aa2ccddd8bc9
MCPolichette <mark@polichette.com>
2018-08-02 21:21:54 -0600

    please work

---
+++ models/example.js
@@ -0,0 +1,7 @@
+module.exports = function(sequelize, DataTypes) {
+  var Example = sequelize.define("Example", {
+    text: DataTypes.STRING,
+    description: DataTypes.TEXT
+  });
+  return Example;
+};
---
+++ routes/htmlRoutes.js
@@ -0,0 +1,27 @@
+var db = require("../models");
+
+module.exports = function(app) {
+  // Load index page
+  app.get("/", function(req, res) {
+    db.Example.findAll({}).then(function(dbExamples) {
+      res.render("index", {
+        msg: "Welcome!",
+        examples: dbExamples
+      });
+    });
+  });
+
+  // Load example page and pass in an example by id
+  app.get("/example/:id", function(req, res) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+      res.render("example", {
+        example: dbExample
+      });
+    });
+  });
+
+  // Render 404 page for any unmatched routes
+  app.get("*", function(req, res) {
+    res.render("404");
+  });
+};
