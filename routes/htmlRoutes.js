faea3916c2756717bb0d823fcae8fb15de21e0cc
MCPolichette <mark@polichette.com>
2018-08-04 19:49:23 -0600

    no progress

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,4 +1,5 @@
 var db = require("../models");
+console.log(db);
 
 module.exports = function(app) {
   // Load index page
@@ -13,7 +14,9 @@ module.exports = function(app) {
 
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function(
+      dbExample
+    ) {
       res.render("example", {
         example: dbExample
       });
