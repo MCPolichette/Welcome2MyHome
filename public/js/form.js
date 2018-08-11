787b258c2a06bf11b969de0e6690f651fc7f3fde
MCPolichette <mark@polichette.com>
2018-08-11 17:15:36 -0600

    more forms

---
+++ public/js/form.js
@@ -0,0 +1,10 @@
+// event listener for Contact submit button.
+// on-click {
+// take fields from inputs and put them into an object
+// insert that object into an array
+// display array of objects on screen
+
+// on click form submit,
+// take array and for loop insert array of objects into contact form
+
+// pull other form Functions from index.js to clean up code in the future
