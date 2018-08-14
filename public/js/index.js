e673df6d61f071a5c7b739bdf3b979d7d219f679
MCPolichette <mark@polichette.com>
2018-08-13 19:51:46 -0600

    Between button attempts

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,3 @@
-
 var $submitBtn = $("#form-submit");
 var $houseList = $("#house-list");
 var CLOUDINARY_URL = "https://api.cloudinary.com/v1_1/welcome2myhome/upload";
@@ -6,6 +5,7 @@ var CLOUDINARY_UPLOAD_PRESET = "j8seyt9p";
 var imagePreview = document.getElementById("img-preview");
 var fileUpload = document.getElementById("file-upload");
 var tempUrl;
+var newId = "";
 // $.cloudinary.config({ cloud_name: 'welcome2myhome', secure: true});
 
 fileUpload.addEventListener("change", function(event) {
@@ -41,9 +41,12 @@ var API = {
       },
       type: "POST",
       url: "api/houses",
-      data: JSON.stringify(house)
-      // var house is being stringified before leading into saveHouse function
-      // original data: JSON.stringify(house)
+      data: JSON.stringify(house),
+      success: function(result) {
+        // remove these after success
+        console.log("TEST");
+        newId = result.id;
+      }
     });
   },
   getHouses: function() {
@@ -84,32 +87,32 @@ var refreshHouses = function() {
       return $li;
     });
     $("#house-name").val(),
-    tempUrl,
-    $("#house-city").val(),
-    $("#house-state").val(),
-    $("#house-zip").val(),
-    $("#house-directions").val(),
-    $("#house-owner").val(),
-	  $("#house-address").val(),
-    $("#house-phone").val(), 
-    $("#house-email").val(),
-    $("#host-notes").val(),
-    $("#house-wifi-network").val(),
-    $("#house-wifi-password").val(), 
-    // $("#house-alarm-key").val(),  
-    // $("#house-description").val(), 
-    $("#departure-date").val(),
-    $("#return-date").val(),
-    $("#trip-destination").val(),
-    $("#trip-phone").val(),
-    $("#trip-address").val(),
-    $("#trip-state").val(),
-    $("#trip-country").val(),
-    $("#trip-notes").val(),
-    $("#house-tv-directions").val(),
-    $("#house-maintenance").val(),
-    $("#house-additional-rules").val(),
-    $houseList.append($houses);
+      tempUrl,
+      $("#house-city").val(),
+      $("#house-state").val(),
+      $("#house-zip").val(),
+      $("#house-directions").val(),
+      $("#house-owner").val(),
+      $("#house-address").val(),
+      $("#house-phone").val(),
+      $("#house-email").val(),
+      $("#host-notes").val(),
+      $("#house-wifi-network").val(),
+      $("#house-wifi-password").val(),
+      // $("#house-alarm-key").val(),
+      // $("#house-description").val(),
+      $("#departure-date").val(),
+      $("#return-date").val(),
+      $("#trip-destination").val(),
+      $("#trip-phone").val(),
+      $("#trip-address").val(),
+      $("#trip-state").val(),
+      $("#trip-country").val(),
+      $("#trip-notes").val(),
+      $("#house-tv-directions").val(),
+      $("#house-maintenance").val(),
+      $("#house-additional-rules").val(),
+      $houseList.append($houses);
   });
 };
 
@@ -121,32 +124,78 @@ var handleFormSubmit = function(event) {
   // photoUpload();
 
   var house = {
-    place_name: $("#place-name").val().trim(),
+    place_name: $("#place-name")
+      .val()
+      .trim(),
     place_photo: tempUrl,
-    place_city: $("#house-city").val().trim(),
-    place_state: $("#house-state").val().trim(),
-    place_zip: $("#house-zip").val().trim(),
-    house_directions: $("#house-directions").val().trim(),
-    host_name: $("#house-owner").val().trim(),
-	  host_address: $("#house-address").val().trim(),
-    host_phone: $("#house-phone").val().trim(), 
-    host_email: $("#house-email").val().trim(),
-    host_notes:  $("#host-notes").val().trim(),
-    wifi_network: $("#house-wifi-network").val().trim(),
-    wifi_password: $("#house-wifi-password").val().trim(), 
-    // house_alarm_pw: $("#house-alarm-key").val().trim(),  
-    // house_info: $("#house-description").val().trim(), 
-    departure_date: $("#departure-date").val().trim(),
-    return_date: $("#return-date").val().trim(),
-    trip_destination: $("#trip-destination").val().trim(),
-    trip_phone: $("#trip-phone").val().trim(),
-    trip_address: $("#trip-address").val().trim(),
-    trip_state: $("#trip-state").val().trim(),
-    trip_country: $("#trip-country").val().trim(),
-    trip_notes: $("#trip-notes").val().trim(),
-    tv_directions: $("#house-tv-directions").val().trim(),
-    house_maintenance: $("#house-maintenance").val().trim(),
-    house_rules: $("#house-additional-rules").val().trim()
+    place_city: $("#house-city")
+      .val()
+      .trim(),
+    place_state: $("#house-state")
+      .val()
+      .trim(),
+    place_zip: $("#house-zip")
+      .val()
+      .trim(),
+    house_directions: $("#house-directions")
+      .val()
+      .trim(),
+    host_name: $("#house-owner")
+      .val()
+      .trim(),
+    host_address: $("#house-address")
+      .val()
+      .trim(),
+    host_phone: $("#house-phone")
+      .val()
+      .trim(),
+    host_email: $("#house-email")
+      .val()
+      .trim(),
+    host_notes: $("#host-notes")
+      .val()
+      .trim(),
+    wifi_network: $("#house-wifi-network")
+      .val()
+      .trim(),
+    wifi_password: $("#house-wifi-password")
+      .val()
+      .trim(),
+    // house_alarm_pw: $("#house-alarm-key").val().trim(),
+    // house_info: $("#house-description").val().trim(),
+    departure_date: $("#departure-date")
+      .val()
+      .trim(),
+    return_date: $("#return-date")
+      .val()
+      .trim(),
+    trip_destination: $("#trip-destination")
+      .val()
+      .trim(),
+    trip_phone: $("#trip-phone")
+      .val()
+      .trim(),
+    trip_address: $("#trip-address")
+      .val()
+      .trim(),
+    trip_state: $("#trip-state")
+      .val()
+      .trim(),
+    trip_country: $("#trip-country")
+      .val()
+      .trim(),
+    trip_notes: $("#trip-notes")
+      .val()
+      .trim(),
+    tv_directions: $("#house-tv-directions")
+      .val()
+      .trim(),
+    house_maintenance: $("#house-maintenance")
+      .val()
+      .trim(),
+    house_rules: $("#house-additional-rules")
+      .val()
+      .trim()
   };
   // console.log(house);
   // if (!(house.place_name && house.house_info)) {
@@ -154,9 +203,16 @@ var handleFormSubmit = function(event) {
   //   return;
   // }
   API.saveHouse(house).then(function() {
+    //===========working here ========================= example below==============================================
+    $("#linkToGuestView")
+      .attr("href", "/guest_view/" + house.id)
+      .then(function() {
+        $("#confirmation-modal").modal("show");
+        console.log("show modal");
+      });
     refreshHouses();
   });
-  // take fields from inputs and put them into an object
+  // take fields from .inputs and put them into an object
   // insert that object into an array
   // display array of objects on screen
   module.exports = function(sequelize, DataTypes) {
